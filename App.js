9a13b996d52f628d10b59b79c5a123e09b9f738c
MuhammedGazi <76705603+MuhammedGazi@users.noreply.github.com>
2023-12-19 22:32:06 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'; 
+import { View, Text, Image, TouchableOpacity, 
+	StyleSheet } from 'react-native'; 
+import FlipCard from 'react-native-flip-card'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => { 
+	const [isFlipped, setIsFlipped] = useState(false); 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	const toggleFlip = () => { 
+		setIsFlipped(!isFlipped); 
+	}; 
+
+	return ( 
+		<View style={styles.container}> 
+			<Text style={styles.heading}> Geeksforgeeks </Text> 
+			<FlipCard 
+				style={[styles.card, styles.shadow]} 
+				friction={6} 
+				perspective={1000} 
+				flipHorizontal={true} 
+				flipVertical={false} 
+				flip={isFlipped} 
+				clickable={false} 
+			> 
+				{/* Front */} 
+				<View style={styles.imageContainer}> 
+					<Image 
+						source={{ uri: 
+ }} 
+						style={styles.image} 
+					/> 
+				</View> 
+
+				{/* Back */} 
+				<View style={styles.imageContainer}> 
+					<Image 
+						source={{ uri: 
+ }} 
+						style={styles.image} 
+					/> 
+				</View> 
+			</FlipCard> 
+
+			<TouchableOpacity 
+				style={styles.button} 
+				onPress={toggleFlip} 
+			> 
+				<Text style={styles.buttonText}>Flip</Text> 
+			</TouchableOpacity> 
+		</View> 
+	); 
+}; 
+
+const styles = StyleSheet.create({ 
+	container: { 
+		flex: 1, 
+		alignItems: 'center', 
+		justifyContent: 'center', 
+		margin: 30, 
+	}, 
+	heading: { 
+		marginBottom: 30, 
+		color: "green", 
+		fontSize: 30, 
+		fontWeight: "bold", 
+	}, 
+	card: { 
+		width: 300, 
+		height: 200, 
+		borderRadius: 30, 
+		backgroundColor: 'grey', 
+
+		
+		overflow: 'hidden', 
+	}, 
+	shadow: { 
+		shadowColor: 'red', 
+		shadowOpacity: 1, 
+		shadowRadius: 15, 
+		shadowOffset: { width: 0, height: 0 }, 
+	}, 
+	imageContainer: { 
+		flex: 1, 
+		alignItems: 'center', 
+		justifyContent: 'center', 
+	}, 
+	image: { 
+		width: "100%", 
+		height: "100%", 
+		borderRadius: 4, 
+	}, 
+	button: { 
+		backgroundColor: 'green', 
+		padding: "10px 30px 10px 30px", 
+		borderRadius: 5, 
+		marginTop: 20, 
+	}, 
+	buttonText: { 
+		fontSize: 20, 
+		fontWeight: 'bold', 
+		textAlign: 'center', 
+		color: "white", 
+	}, 
+}); 
+
+export default App;
