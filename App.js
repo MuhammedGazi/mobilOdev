bf6ea2c27a8a55de0231993a188480da30a9a3b5
MuhammedGazi <76705603+MuhammedGazi@users.noreply.github.com>
2023-12-20 18:31:09 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,58 +1,110 @@
-import React, { useState } from 'react'; 
-import { View, Text, Image, TouchableOpacity, 
-	StyleSheet } from 'react-native'; 
-import FlipCard from 'react-native-flip-card'; 
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import * as FileSystem from 'expo-file-system';
 
-const App = () => { 
-	const [isFlipped, setIsFlipped] = useState(false); 
+const wordsData = [
+  { "word": "Apple", "definition": "A red or green fruit with a sweet taste.", "id": 1 },
+  { "word": "Car", "definition": "A vehicle with four wheels that is powered by an engine.", "id": 2 },
+  { "word": "Sun", "definition": "The star that is the central body of the solar system.", "id": 3 },
+];
 
-	const toggleFlip = () => { 
-		setIsFlipped(!isFlipped); 
-	}; 
 
-	return ( 
-		<View style={styles.container}> 
-			<Text style={styles.heading}> flip card </Text> 
-			<FlipCard 
-				style={[styles.card, styles.shadow]} 
-				friction={6} 
-				perspective={1000} 
-				flipHorizontal={true} 
-				flipVertical={false} 
-				flip={isFlipped} 
-				clickable={false} 
-			> 
-				{/* Front */} 
-				<View style={styles.imageContainer}> 
-					<Image 
-						source={{ uri: 
- }} 
-						style={styles.image} 
-					/> 
-				</View> 
 
-				{/* Back */} 
-				<View style={styles.imageContainer}> 
-					<Image 
-						source={{ uri: 
- }} 
-						style={styles.image} 
-					/> 
-				</View> 
-			</FlipCard> 
 
-			<TouchableOpacity 
-				style={styles.button} 
-				onPress={toggleFlip} 
-			> 
-				<Text style={styles.buttonText}>Flip</Text> 
-			</TouchableOpacity> 
-		</View> 
-	); 
-}; 
+// Uygulama başladığında veya belirli bir yerde çağrarak dosyayı oluşturun
+const createLikedWordsFile = async () => {
+  const likedWordsFilePath = `${FileSystem.documentDirectory}likedWords.json`;
 
-const styles = StyleSheet.create({ 
-	container: { 
+  try {
+    // Dosya mevcut değilse oluşturun
+    await FileSystem.writeAsStringAsync(likedWordsFilePath, '[]');
+
+
+    const likedWordsContent = await FileSystem.readAsStringAsync(likedWordsFilePath);
+    console.log('likedWords.json content:', likedWordsContent);
+
+  } catch (error) {
+    console.error('Error creating likedWords.json:', error);
+  }
+
+
+
+};
+
+createLikedWordsFile();
+
+
+
+const FlipCardApp = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const toggleCard = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  const nextCard = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % wordsData.length);
+    setIsFlipped(false);
+  };
+
+  const likeWord = async () => {
+    try {
+      const likedWordsFilePath = `${FileSystem.documentDirectory}likedWords.json`;
+      const likedWordsContent = await FileSystem.readAsStringAsync(likedWordsFilePath);
+      let likedWords = JSON.parse(likedWordsContent);
+      const currentWord = wordsData[currentIndex].word;
+  
+      // Check if the word is not already liked
+      if (!likedWords.includes(currentWord)) {
+        likedWords.push(currentWord);
+  
+        // Update the likedWords file
+        await FileSystem.writeAsStringAsync(likedWordsFilePath, JSON.stringify(likedWords));
+  
+        console.log('Liked word added:', currentWord); // Yeni log eklenen beğenilen kelimeyi gösterir
+  
+        Alert.alert('Success', `${currentWord} added to liked words.`);
+      } else {
+        Alert.alert('Info', `${currentWord} is already in liked words.`);
+      }
+    } catch (error) {
+      console.error('Error liking word:', error);
+      Alert.alert('Error', 'An error occurred while liking word.');
+    }
+  };
+  
+
+  useEffect(() => {
+    setCurrentIndex(0);
+    setIsFlipped(false);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={toggleCard}>
+        <View style={[styles.card, isFlipped && styles.flipped]}>
+          <Text style={[styles.text, isFlipped && styles.flippedText]}>
+            {isFlipped ? wordsData[currentIndex].definition : wordsData[currentIndex].word}
+          </Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={nextCard}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Next Card</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={likeWord}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Like Word</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { 
 		flex: 1, 
 		alignItems: 'center', 
 		justifyContent: 'center', 
@@ -101,6 +153,6 @@ const styles = StyleSheet.create({
 		textAlign: 'center', 
 		color: "white", 
 	}, 
-}); 
+});
 
-export default App;
+export default FlipCardApp;
