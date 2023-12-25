47b4d715021e6a3ffad6926da1366c7588f3ea25
MuhammedGazi <76705603+MuhammedGazi@users.noreply.github.com>
2023-12-25 20:47:56 +0300

    Update App.js

--- App.js
+++ App.js
@@ -116,14 +116,100 @@ const ProfileScreen = ({ navigation }) => {
 
   useEffect(() => {
     loadLikedWords();
+  }, import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as FileSystem from 'expo-file-system';
+
+const wordsData = [
+  { "word": "Elma", "definition": "Tatlı bir tada sahip kırmızı veya yeşil meyve.", "id": 1 },
+  { "word": "Araba", "definition": "Dört tekerlekli, motorla çalışan bir taşıt aracı.", "id": 2 },
+  { "word": "Güneş", "definition": "Güneş sisteminin merkezinde bulunan yıldız.", "id": 3 },
+];
+
+const HomeScreen = ({ navigation }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [wrongAnswers, setWrongAnswers] = useState(0);
+  const [likedWords, setLikedWords] = useState([]);
+
+  const toggleCard = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  const nextCard = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % wordsData.length);
+    setIsFlipped(false);
+  };
+
+  const likeWord = () => {
+    const currentWord = wordsData[currentIndex].word;
+    setLikedWords((prevLikedWords) => [...prevLikedWords, currentWord]);
+    Alert.alert('Başarılı', `${currentWord} beğenilen kelimelere eklendi.`);
+  };
+
+  const handleWrongAnswer = () => {
+    setWrongAnswers(wrongAnswers + 1);
+    Alert.alert('Yanlış Cevap', 'Tekrar deneyin!');
+  };
+
+  useEffect(() => {
+    const loadLikedWords = async () => {
+      try {
+        const likedWordsFilePath = `${FileSystem.documentDirectory}likedWords.json`;
+        const likedWordsContent = await FileSystem.readAsStringAsync(likedWordsFilePath);
+        const parsedLikedWords = JSON.parse(likedWordsContent);
+        setLikedWords(parsedLikedWords);
+      } catch (error) {
+        console.error('Beğenilen kelimeleri yükleme hatası:', error);
+      }
+    };
+
+    loadLikedWords();
   }, []);
 
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={toggleCard}>
+        <View style={[styles.card, isFlipped && styles.flipped]}>
+          <Text style={[styles.text, isFlipped && styles.flippedText]}>
+            {isFlipped ? wordsData[currentIndex].definition : wordsData[currentIndex].word}
+          </Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={nextCard}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Sonraki Kart</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={likeWord}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Kelimeyi Beğen</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleWrongAnswer}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Yanlış Cevap</Text>
+        </View>
+      </TouchableOpacity>
+      <Text style={styles.buttonText}>Yanlış Cevap Sayısı: {wrongAnswers}</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Profil')}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Profili Görüntüle</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const ProfileScreen = ({ likedWords }) => {
+  return (
+    <View style={styles.container}>
       <Text style={styles.heading}>Beğenilen Kelimeler</Text>
       <View>
-        {likedWords.map((word) => (
-          <Text key={word}>{word}</Text>
+        {likedWords.map((word, index) => (
+          <Text key={index}>{word}</Text>
         ))}
       </View>
     </View>
@@ -133,25 +219,13 @@ const ProfileScreen = ({ navigation }) => {
 const Stack = createStackNavigator();
 
 const App = () => {
-  const createLikedWordsFile = async () => {
-    const likedWordsFilePath = `${FileSystem.documentDirectory}likedWords.json`;
-
-    try {
-      await FileSystem.writeAsStringAsync(likedWordsFilePath, '[]');
-      const likedWordsContent = await FileSystem.readAsStringAsync(likedWordsFilePath);
-      console.log('likedWords.json içeriği:', likedWordsContent);
-    } catch (error) {
-      console.error('Beğenilen kelimeler dosyası oluşturma hatası:', error);
-    }
-  };
-
-  createLikedWordsFile();
-
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="AnaSayfa">
         <Stack.Screen name="AnaSayfa" component={HomeScreen} />
-        <Stack.Screen name="Profil" component={ProfileScreen} />
+        <Stack.Screen name="Profil">
+          {(props) => <ProfileScreen {...props} likedWords={[]} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -194,3 +268,12 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
     marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+});
+
+export default App;
